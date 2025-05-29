6aef2286ae8e7331a15671227e3f17b62c8f47df
Stefan Hancar <stefan@hancar.sk>
2025-05-29 12:58:24 +0200

    add update

--- src/app/events/[eventId]/page.tsx
+++ src/app/events/[eventId]/page.tsx
@@ -1,23 +1,24 @@
 import { getEvents, Event } from "@/app/lib/firestore";
 
-type PageProps = {
-  params: {
-    eventId: string;
-  };
-};
-
-export default async function EventDetailPage({ params }: PageProps) {
+export default async function EventDetailPage({
+  params,
+}: {
+  params: { eventId: string };
+}) {
   const events: Event[] = await getEvents();
   const event = events.find((e) => e.id === params.eventId);
 
   if (!event) return <p className="p-4">Event not found</p>;
 
+  const eventDate =
+    event.date instanceof Date
+      ? event.date
+      : new Date(event.date.seconds * 1000);
+
   return (
     <main className="p-4 max-w-2xl mx-auto">
       <h1 className="text-3xl font-bold mb-2">{event.title}</h1>
-      <p className="text-gray-700 mb-2">
-        {new Date(event.date.seconds * 1000).toLocaleDateString()}
-      </p>
+      <p className="text-gray-700 mb-2">{eventDate.toLocaleDateString()}</p>
       <p className="text-gray-700 mb-4">{event.description}</p>
     </main>
   );
